96c9eb78ec5922730574c77f67b183e3eba32af1
Esam-Ankir <eankir@hotmail.com>
2022-09-04 03:39:02 +0300

    redux

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+import Categories from './components/categories/categories'
+
+export default function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Categories />
+      <Footer></Footer>
+    </div>
+  )
+}
